371b29785368c46110f4e8024fef9f29502a9b0f
leopardciaw <47556424+leopardciaw@users.noreply.github.com>
2020-02-28 00:53:50 +0800

    Update diy.sh
    gg

--- diy.sh
+++ diy.sh
@@ -7,5 +7,5 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
-git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/
-git clone https://github.com/leopardciaw/luci-app-koolddns.git package/lean/
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
+git clone https://github.com/leopardciaw/luci-app-koolddns.git package/lean/luci-app-koolddns
