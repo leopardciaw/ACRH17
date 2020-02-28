f61eb664dc6eaabcea9fd755078ab965b9afeeee
“leopardciaw” <quanyongw@gmail.com>
2020-02-28 13:18:30 +0800

    update，2020-2-28 13:18:21

--- diy.sh
+++ diy.sh
@@ -7,5 +7,16 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+
+# 删除原主题包
+ rm -rf package/lean/luci-theme-argon
+# rm -rf openwrt/package/lean/luci-theme-netgear
+
+# 添加新的主题包
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
+# git clone https://github.com/leopardciaw/luci-theme-netgear.git package/lean/luci-theme-netgear
+
+#增加koolddns
 git clone https://github.com/leopardciaw/luci-app-koolddns.git package/lean/luci-app-koolddns
+
+
