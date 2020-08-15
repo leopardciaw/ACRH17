c359cd7b2f51f4d287e8a16441472956d41f20b0
leopardciaw <quanyongw@gmail.com>
2020-08-15 16:25:16 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -4,6 +4,7 @@
 # Lisence: MIT
 # Author: leopard
 # Blog: https://p3terx.com
+# 原来
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
